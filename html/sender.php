4ab4b56e9255c6edd9de3f95d3f6ae8b25efd261
eco-kit-build <106723525+eco-kit-build@users.noreply.github.com>
2022-06-02 20:08:05 +0800

    Update sender.php

--- html/sender.php
+++ html/sender.php
@@ -1,40 +1,10 @@
-<?php 
-
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$email = $_POST['message'];
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = '000333555666@mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'asdasdasd.xcvxcvxcvxc.71@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'jarajaramako89Dim'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
-}
+<?
+$name = @ trim ($_POST['name']);
+$email = @ trim ($_POST['email']);
+$message = @ trim ($_POST['message']);
+mail ("000333555666@mail.ru",
+      "Новое сообщение с сайта (от : $name)",
+      " Имя:$name \n Email:$email \n Телефон: $message",
+      "Content-type:text/plain; charset=utf-8");
+header ("Location: http://трутрутру.ru/");
 ?>
