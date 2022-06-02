9fd93e1cbc298b0c3d96ccc9dcd61b6ffc992fb7
eco-kit-build <106723525+eco-kit-build@users.noreply.github.com>
2022-06-02 18:25:33 +0800

    Delete java directory

--- java/gallery.js
+++
@@ -1,7 +0,0 @@
-$(".gallery-list").magnificPopup({
-    delegate: "a",
-    type: "image",
-    gallery: {
-        enabled: true
-    }
-});
--- java/java.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function (){
-    $('.burger').click(function (event){
-        $('.burger, .nav, .secret_box').toggleClass('active');
-    });
-});
