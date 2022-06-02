7a21de9b3f1b85b992606b019c8405176ad7f287
eco-kit-build <106723525+eco-kit-build@users.noreply.github.com>
2022-06-02 18:21:29 +0800

    Create box_information.js

---
+++ java/box_information.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+    $('.title_box_1').click(function (event){
+        $('.title_box_1, .text_information_box_1').toggleClass('active');
+    });
+});
+$(document).ready(function (){
+    $('.title_box_2').click(function (event){
+        $('.title_box_2, .text_information_box_2').toggleClass('active');
+    });
+});
+$(document).ready(function (){
+    $('.title_box_3').click(function (event){
+        $('.title_box_3, .text_information_box_3').toggleClass('active');
+    });
+});
